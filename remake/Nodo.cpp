c09c211d343bc0fceac170b39344375f85ae6995
leddkire <leddkire@gmail.com>
2014-05-07 19:03:32 -0430

    Operaciones de Nodo nuevo

--- remake/Nodo.cpp
+++ remake/Nodo.cpp
@@ -72,90 +72,89 @@ public:
 	}
 
 	bitset<8> getF(){
-
+		bitset<86> temp;
+		temp = datos >> 78 & bitset<86>(255);
+		return bitset<8>(temp.to_ulong());
 	}
 
 	bitset<8> getProf(){
-
+		bitset<86> temp;
+		temp = datos >> 70 & bitset<86>(255);
+		return bitset<8>(temp.to_ulong());
 	}
 
 	bitset<8> getCosto(){
-		return profundidad;
+		bitset<86> temp;
+		temp = datos >> 70 & bitset<86>(255);
+		return bitset<8>(temp.to_ulong());
 	}
 
 	bitset<2> getAccion(){
-
+		bitset<86> temp;
+		temp = datos >> 68 & bitset<86>(3);
+		return bitset<2>(temp.to_ulong());
 	}
 
 	bitset<4> getUbicacion0(){
-
+		bitset<86> temp;
+		temp = datos >> 64 & bitset<86>(15);
+		return bitset<4>(temp.to_ulong());
 	}
 
 	bitset<64> getEstado(){
-
+		bitset<86> temp;
+		bitset<64> temp2;
+		temp = datos & bitset<86>((2^32)-1);
+		temp2 = bitset<64>(temp.to_ulong());
+		temp2 <<= 32;
+		datos >>= 32;
+		temp = datos & bitset<86>((2^32)-1);
+		temp2 = (bitset<64>(temp.to_ulong())) | (temp2 & ~(bitset<64>((2^32) - 1)));
+		return temp2;
 	}
 
 	void setF(bitset<8> d){
-
+		bitset<86> temp = bitset<86>(d.to_ulong());
+		datos = (temp << 78) | (datos & ~(bitset<8>(255)<< 78));
 	}
 
 	void setProf(bitset<8> d){
 
+		bitset<86> temp = bitset<86>(d.to_ulong());
+		datos = (temp << 70) | (datos & ~(bitset<8>(255)<< 70));
 	}
 
 	void setAccion(bitset<2> d){
 
+		bitset<86> temp = bitset<86>(d.to_ulong());
+		datos = (temp << 68) | (datos & ~(bitset<2>(3)<< 68));
 	}
 
 	void setUbicacion0(bitset<4> d){
 
+		bitset<86> temp = bitset<86>(d.to_ulong());
+		datos = (temp << 64) | (datos & ~(bitset<8>(255)<< 64));
 	}
 
 	void setEstado(bitset<64> d){
+		bitset<64> bit= bitset<64>((2^32)-1);
+		bit &= d;
+		bitset<86> bit2 = bitset<86>(bit.to_ulong());
+		bit2 <<= 32;
+		datos = bit2 | (datos & ~(bitset<86>(2^32-1)) << 32);
+
+		//Meter segunda mitad del estado
+		d >>= 32;
+		bit= bitset<64>((2^32)-1);
+		bit &= d;
+		bit2 = bitset<86>(bit.to_ulong());
+		datos = bit2 | (datos & ~(bitset<86>(2^32-1)));
 		
 	}
 
 };
 
 
-
-/*
-class Nodo{
-public:
-	Estado* estado;
-	Nodo* padre;
-	Accion* accion;
-	int profundidad;
-	int f; //Valor sumado de el costo mas la heuristica del estado
-	Nodo(){
-		estado = NULL;
-		padre = NULL;
-		accion = NULL;
-		profundidad = -1;
-		f = -1;
-	}
-	//Constructor equivalente a make_root_node()
-	Nodo(Estado* e){
-		estado = e;
-		padre = NULL;
-		accion = NULL;
-		profundidad = 0;
-		f = 0;
-	}
-	//Constructor equivalente a make_node(Estado,Nodo,Accion)
-	Nodo (Estado* e, Nodo *n, Accion* a){
-		estado = e;
-		padre = n;
-		accion = a;
-		profundidad = n -> profundidad + 1;
-		f = 0;
-	}
-
-	int getCosto(){
-		return profundidad;
-	}
-};
-*/
 
 class comparar{//Clase para la comparacion de costos de nodo
 
